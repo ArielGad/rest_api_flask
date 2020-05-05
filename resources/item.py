89da4a37eec9fd5ac926a77bece9f7a36bf2e096
ArielGad <ariel.gad4@gmail.com>
2020-05-05 14:31:53 +0300

    @jwt_optional

--- resources/item.py
+++ resources/item.py
@@ -1,6 +1,6 @@
 from flask_restful import Resource, reqparse
 # from flask_jwt import jwt_required
-from flask_jwt_extended import jwt_required, get_jwt_claims
+from flask_jwt_extended import jwt_required, get_jwt_claims, jwt_optional, get_jwt_identity
 from models.item import ItemModel
 
 
@@ -59,5 +59,14 @@ class Item(Resource):
 
 
 class ItemList(Resource):
+    @jwt_optional
     def get(self):
-        return {'items': [item.json() for item in ItemModel.find_all()]}
+        user_id = get_jwt_identity()  # this method will give us whatever we saved in access_token as the identity
+        items = [item.json() for item in ItemModel.find_all()]
+        if user_id:
+            return {'items': items}, 200
+
+        return {
+            'items': [item.get('name') for item in items],
+            'message': 'More data available if you log in.'
+        }, 200
